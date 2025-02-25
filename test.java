ebae212a275d1fdb8c4b4e04232c0154b867fd0d
om kasabekar <omkasabekar19@gmail.com>
2025-02-25 20:36:59 +0530

    deleted test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-class test{
-    public static void main(String args[]){
-        System.err.println("Hello world");
-        System.err.println("I am Om");
-        System.err.println("created Om branch");
-    }
-}
