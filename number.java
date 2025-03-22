b6cd34f3afccab6ee6547e4877cda6394c624e68
om kasabekar <omkasabekar19@gmail.com>
2025-03-22 12:58:50 +0530

    removed unwanted files

--- number.java
+++
@@ -1,5 +0,0 @@
-class number{
-    public static void main(String[] args) {
-        System.out.println("I am Om testing the branch");
-    }
-}
--- testing.java
+++
@@ -1,5 +0,0 @@
-class testing{
-    public static void main(String args[]){
-        System.out.println("I am testing the code way branch");
-    }
-}
